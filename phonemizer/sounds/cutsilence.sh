c705e55d2bb9a371c14acdfb20987bc57821bc43
norman <norman@thinkpad440.local>
2020-02-07 17:18:11 +0100

    neue sounds

--- phonemizer/sounds/cutsilence.sh
+++ phonemizer/sounds/cutsilence.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function cutsilence {
+		echo ffmpeg -i $1 -af "silenceremove=start_periods=0:start_duration=0:start_threshold=-60dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=0:start_duration=0:start_threshold=-50dB:detection=peak,aformat=dblp,areverse" "REDONE-$1"
 		ffmpeg -i $1 -af "silenceremove=start_periods=0:start_duration=0:start_threshold=-60dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=0:start_duration=0:start_threshold=-50dB:detection=peak,aformat=dblp,areverse" "REDONE-$1"
 		mv "REDONE-$1" $1
 
@@ -11,5 +12,11 @@ if [ $# -eq 0 ]; then
 		cutsilence $i
 	done
 else
-	cutsilence $1
+	if [ $1 = "git" ]; then
+		for i in $(git status . | grep ".ogg" | sed -e 's/^\s*//' | sed -e 's/"?\s*$//' | sed -e 's/^"//' | sed -e 's/"\s*$//'); do
+			cutsilence $i
+		done
+	else
+		cutsilence $1
+	fi
 fi
